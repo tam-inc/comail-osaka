6246841f469f2bc443b3a1cd9c418812b858583f
matsuo@TAM <matsuo@tam-tam.co.jp>
2016-08-30 16:17:32 +0900

    riceモデル 一通り作成

--- app/Rice.php
+++ app/Rice.php
@@ -2,22 +2,118 @@
 
 namespace App;
 
+use DB;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Rice extends Model
 {
+    protected $table = 'rice';
+
+    /** @var array */
+    protected $guarded = ['id', 'ricer'];
+
+
+    /**
+     * 本日の担当を返す
+     * @param null $date
+     * @return mixed
+     */
+    public function getRicer($date = null)
+    {
+        $date = $date ?? Carbon::today()->toDateString();
+
+        return $this
+            ->where('date', $date)
+            ->where('ricer', '=', true)
+            ->first();
+    }
+
     /**
      * 本日抽選済みかどうかを返す
      * @param null $date
      * @return bool
      */
-    static function isSelected($date = null)
+    public function isSelected($date = null)
+    {
+        return ($this->getRicer($date) != null);
+    }
+
+    /**
+     * 本日のメンバーリストを返す
+     * @param null $date
+     * @return mixed
+     */
+    public function getTodayMembers($date = null)
     {
         $date = $date ?? Carbon::today()->toDateString();
 
-        $ricer = static::where('date', '=', $date)->where('ricer', '=', true)->count();
+        return $this
+            ->where('date', $date)
+            ->where('volume', '>', 0)
+            ->orderBy('ricer', 'desc')
+            ->inRandomOrder()
+            ->pluck('name');
+    }
 
-        return ($ricer > 0);
+    /**
+     * 本日の分量を返す
+     * @param null $date
+     * @return mixed
+     */
+    public function getVolume($date = null)
+    {
+        $date = $date ?? Carbon::today()->toDateString();
+
+        return $this
+            ->where('date', $date)
+            ->where('volume', '>', 0)
+            ->sum('volume');
+    }
+
+    /**
+     * 最近20日間の担当者を返す
+     * @return mixed
+     */
+    public function getResult()
+    {
+        return $this
+            ->where('date', '>=', Carbon::parse('-20days')->toDateString())
+            ->where('ricer', true)
+            ->orderBy('date', 'desc')
+            ->pluck('name', 'date');
+    }
+
+    /**
+     * 担当者をえらぶ
+     */
+    public function pickup($date = null)
+    {
+        $date = $date ?? Carbon::today()->toDateString();
+
+        // 本日の対象者 (過去7日間で担当の少ない人を選ぶ)
+        $ricer = DB::table($this->table)
+            ->select(DB::raw('rice.*, count(rice2.id) as cnt'))
+            ->leftJoin('rice as rice2', function($join) use ($date) {
+
+                // join: 過去7日間で担当した人を抽出
+                $fromDate = Carbon::parse('-7days')->toDateString();
+                $join
+                    ->on('rice.email', '=', 'rice2.email')
+                    ->where('rice2.date', '>=', $fromDate)
+                    ->where('rice2.ricer', true);
+            })
+            ->where('rice.date', '=', $date)
+            ->where('rice.volume', '>', 0)
+            ->groupBy('rice.email')
+            ->orderBy('cnt', 'asc')
+            ->inRandomOrder()
+            ->first();
+
+        if (empty($ricer)) {
+            return false;
+        }
+
+        return $ricer;
     }
 }
--- tests/RiceTest.php
+++ tests/RiceTest.php
@@ -10,6 +10,18 @@ class RiceTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public $Rice;
+
+    /**
+     * @return array
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->Rice = new Rice();
+    }
+
     /**
      * 本日抽選済みかどうか
      * @return void
@@ -27,6 +39,7 @@ class RiceTest extends TestCase
 
         // yesterday: 抽選済み
         factory(Rice::class, 1)->create([
+            'name' => 'riceman',
             'date' => $yesterday,
             'ricer' => true,
         ]);
@@ -36,7 +49,142 @@ class RiceTest extends TestCase
         ]);
 
         // assert
-        $this->assertEquals(Rice::isSelected($today), false);
-        $this->assertEquals(Rice::isSelected($yesterday), true);
+        $this->assertEquals($this->Rice->isSelected($today), false);
+        $this->assertEquals($this->Rice->isSelected($yesterday), true);
+    }
+
+    /**
+     * 最近20日の結果を返す
+     */
+    public function testGetResult()
+    {
+        $data = [
+            [
+                'date'  => Carbon::today()->toDateString(),
+                'ricer' => true,
+            ],
+            [
+                'date'  => Carbon::yesterday()->toDateString(),
+                'ricer' => false,
+            ],
+            [
+                'date'  => Carbon::parse('-20days')->toDateString(),
+                'ricer' => true,
+            ],
+            [
+                'date'  => Carbon::parse('-21days')->toDateString(),
+                'ricer' => true,
+            ],
+        ];
+
+        foreach ($data as $a) {
+            factory(Rice::class)->create($a);
+        }
+
+        $this->assertEquals(count($this->Rice->getResult()), 2);
+    }
+
+    public function testVolume()
+    {
+        $data = [
+            [
+                'date'  => Carbon::today()->toDateString(),
+                'volume' => 1,
+            ],
+            [
+                'date'  => Carbon::today()->toDateString(),
+                'volume' => 2,
+            ],
+            [
+                'date'  => Carbon::today()->toDateString(),
+                'volume' => 3,
+            ],
+            [
+                'date'  => Carbon::yesterday()->toDateString(),
+                'volume' => 4,
+            ],
+        ];
+
+        foreach ($data as $a) {
+            factory(Rice::class)->create($a);
+        }
+
+        $this->assertEquals($this->Rice->getVolume(), 1+2+3);
+    }
+
+    /**
+     * 最近20日の結果を返す
+     */
+    public function testPickup1()
+    {
+        // Bが担当多いのでAが本日の担当に
+        $data = [
+            [
+                'date'  => Carbon::today()->toDateString(),
+                'email' => 'A',
+            ],
+            [
+                'date'  => Carbon::today()->toDateString(),
+                'email' => 'B',
+            ],
+            [
+                'date'  => Carbon::yesterday()->toDateString(),
+                'email' => 'A',
+                'ricer' => false,
+            ],
+            [
+                'date'  => Carbon::yesterday()->toDateString(),
+                'email' => 'B',
+                'ricer' => true,
+            ],
+        ];
+
+        foreach ($data as $a) {
+            factory(Rice::class)->create($a);
+        }
+
+        $this->assertEquals($this->Rice->pickup()->email, 'A');
+
+
+        // Aが担当多いのでBが本日の担当に
+        $data = [
+            [
+                'date'  => Carbon::parse('-3days')->toDateString(),
+                'email' => 'A',
+                'ricer' => true,
+            ],
+            [
+                'date'  => Carbon::parse('-4days')->toDateString(),
+                'email' => 'A',
+                'ricer' => true,
+            ],
+        ];
+
+        foreach ($data as $a) {
+            factory(Rice::class)->create($a);
+        }
+
+        $this->assertEquals($this->Rice->pickup()->email, 'B');
+
+
+        // Bが担当多いけど7日以上前なので、Bが本日の担当に
+        $data = [
+            [
+                'date'  => Carbon::parse('-8days')->toDateString(),
+                'email' => 'B',
+                'ricer' => true,
+            ],
+            [
+                'date'  => Carbon::parse('-9days')->toDateString(),
+                'email' => 'B',
+                'ricer' => true,
+            ],
+        ];
+
+        foreach ($data as $a) {
+            factory(Rice::class)->create($a);
+        }
+
+        $this->assertEquals($this->Rice->pickup()->email, 'B');
     }
 }
