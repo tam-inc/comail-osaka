152cc068ce9df5609bd46e98e3cd7b26e9ae444e
matsuo@TAM <matsuo@tam-tam.co.jp>
2016-09-04 13:25:18 +0900

    BASIC認証

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@
 
 Route::get('/', function () {
     // basic auth
-    switch (env('APP_ENV') == 'production') {
-        case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
-        case $_SERVER['PHP_AUTH_USER'] !== env('AUTH_USER'):
-        case $_SERVER['PHP_AUTH_PW']   !== env('AUTH_PW'):
-            header('WWW-Authenticate: Basic realm="Enter username and password."');
-            header('Content-Type: text/plain; charset=utf-8');
-            die('401 Unauthorized.');
+    if (env('APP_ENV') == 'production') {
+        switch (true) {
+            case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+            case $_SERVER['PHP_AUTH_USER'] !== env('AUTH_USER'):
+            case $_SERVER['PHP_AUTH_PW']   !== env('AUTH_PW'):
+                header('WWW-Authenticate: Basic realm="Enter username and password."');
+                header('Content-Type: text/plain; charset=utf-8');
+                die('401 Unauthorized.');
+        }
     }
 
     $rice = new App\Rice();
